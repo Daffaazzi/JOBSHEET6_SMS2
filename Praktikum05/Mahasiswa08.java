28b0fa81dd7bc43472354270c673b7b768f0df01
Daffaazzi <azzidaffa@gmail.com>
2026-03-31 11:09:15 +0700

    Pushhhhhh 1

---
+++ Praktikum05/Mahasiswa08.java
@@ -0,0 +1,32 @@
+package Praktikum05;
+
+public class Mahasiswa08 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    static Mahasiswa08[] listMhs;
+    static int idx;
+
+    // Konstruktor default
+    Mahasiswa08() {
+
+    }
+    
+    Mahasiswa08(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+   
+}
---
+++ Praktikum05/MahasiswaBerprestasi08.java
@@ -0,0 +1,92 @@
+package Praktikum05;
+import java.util.Scanner;
+public class MahasiswaBerprestasi08 {
+    
+    Mahasiswa08[] listMhs = new Mahasiswa08[5];
+    int idx;
+
+    void tambah(Mahasiswa08 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < idx; i++) {
+            listMhs[i].tampilInformasi();
+            System.out.println("-------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa08 temp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    void selectionSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            Mahasiswa08 temp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = temp;
+        }
+    }
+
+    void insertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa08 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
+
+    void insertionSortDesc() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa08 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) { 
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
+
+    void tambahDariKeyboard() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < listMhs.length; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine(); // konsumsi newline
+            listMhs[i] = new Mahasiswa08(nim, nama, kelas, ipk);
+            System.out.println("------------------------------");
+        }
+        sc.close();
+    }
+}
